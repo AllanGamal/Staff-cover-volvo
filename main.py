d25e125ee3e93fdab7d9a7a2c2178ae8886b7f17
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2022-04-30 22:46:50 +0200

    Defining basic functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Class of object called name that contains name, balance, present and placed
+
 class Fitter:
     def __init__(self, name, competency, present, placed):
         self.name = name
@@ -14,25 +14,16 @@ class Team:
         self.fitter = fitter
 
 
-possibleCombos = []
-
-
-
-
 f1 = Fitter("karen",[2,3,4,8], True, False)
 f2 = Fitter("eye",[2,3,4,8], True, False)
 f3 = Fitter("dani",[2,3,8], True, False)
 f4 = Fitter("pika",[2,4,5,8], True, False)
-f5 = Fitter("man",[9], True, False)
+f5 = Fitter("man",[5], True, False)
 
 t1 = Team("team1", [8], [])
 t2 = Team("team2", [2], [])
 t3 = Team("team3", [3], [])
 t4 = Team("team4", [4,5], [])
-
-
-
-
 
 fitters = []
 teams = []
@@ -43,16 +34,20 @@ teams.append(t3)
 teams.append(t4)
 
 
-stations = []
+# function that returns an array of all the stations
+def get_stations(st):
+    # remove every element in stations
+    st.clear()
+    # add all the stations to stations
+    for team in teams:
+      for station in team.station:
+        st.append(station)
+    return st
 
 # append every element in each team.station to the stations list
-for team in teams:
-    for station in team.station:
-        stations.append(station)
+stations = []
+get_stations(stations)
 print(stations)
-
-
-
 
 # Add the all the object fitters in a loop
 fitters.append(f1)
@@ -62,28 +57,36 @@ fitters.append(f4)
 fitters.append(f5)
 
 
-f3.name = "charlie"
-print(f3.name)
+def nested_arr(st, arr):  
+    # clear arr
+    arr.clear()
+    # loop through all the stations
+    for i in range(len(st)):
+        arr.append([])
+        # add every fitter.name based on shared competency to the array
+        for fitter in fitters:
+            if fitter.present == True: # if the fitter is present
+                for station in fitter.competency:
+                    if station == st[i]:
+                        arr[i].append(fitter.name)
 
-# check in a loop if the name is charle and change it to shitface
-for fitter in fitters:
-    if fitter.name == "charlie":
-        fitter.name = "shitface"
-        print(fitter.name)
+    return arr
+sort_comp = []
+nested_arr(stations, sort_comp)
+print(sort_comp)
 
-st = stations
-tm = []
-
-# If the fitter is present and the competency is in the st list and the fitter is not in the tm list add the fitter to the tm list 
-for fitter in fitters:
-    if fitter.present == True:
-        for station in st:
-            if station in fitter.competency:
-                if fitter not in tm:
-                    tm.append(fitter)
-                    
-
-                
-# print every fitter.name in the list
-for fitter in tm:
-    print(fitter.name)
+# function that tries every combination of single element in the stations list but only one name per station
+def try_combination(st, arr):
+    # clear arr
+    arr.clear()
+    # loop through all the stations
+    for i in range(len(st)):
+        # add every fitter.name based on shared competency to the array
+        for fitter in fitters:
+            if fitter.present == True: # if the fitter is present
+                for station in fitter.competency:
+                    if station == st[i]:
+                        arr.append(fitter.name)
+    return arr
+try_combination(stations, sort_comp)
+print(sort_comp)
